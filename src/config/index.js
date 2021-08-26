57f146d243665347ca11fa0d0fc10265e53daa59
Evandro <34420396+ev4ndropc@users.noreply.github.com>
2021-08-26 16:48:51 -0300

    change api route

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   //Development info
   DEVELOPMENT_MODE: false,
   DEVELOPMENT_BASEURL: 'http://localhost:3333',
-  DEPLOY_BASEURL: 'https://postaltrack.com.br:1212',
+  DEPLOY_BASEURL: 'https://api.postaltrack.com.br',
 
   //Site information
   SITE_NAME: 'Postal Track'
